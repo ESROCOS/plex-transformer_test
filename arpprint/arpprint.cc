bcd71fa8cb0e4724705667d7894819b5ae85eb6e
Benjamin <benjamin.kisliuk@dfki.de>
2018-08-22 18:35:17 +0000

    added printer

---
+++ arpprint/arpprint.cc
@@ -0,0 +1,42 @@
+/* User code: This file will not be overwritten by TASTE. */
+
+#include "arpprint.h"
+#include <iostream>
+#include "base_support/Base-samples-RigidBodyStateConvert.hpp"
+#include "base_support/OpaqueConversion.hpp"
+#include <Eigen/Core>
+
+void arpprint_startup()
+{
+    /* Write your initialization code here,
+       but do not make any call to a required interface. */
+}
+
+// EIGEN HELPER METHODS
+void to4d(Eigen::Matrix3d r, Eigen::Vector3d t, Eigen::Matrix4d & h){
+	
+  h<< r(0,0), r(0,1), r(0,2), t[0],
+      r(1,0), r(1,1), r(1,2), t[1],
+      r(2,0), r(2,1), r(2,2), t[2],
+          0,      0,      0,    1;
+}
+
+void arpprint_PI_print_rbs(const asn1SccBase_samples_RigidBodyState *IN_pose)
+{
+  base::Vector3d t;
+  base::Quaterniond q;
+  Eigen::Matrix3d r;
+  // extract orientation / translation
+  asn1Scc_Vector3d_fromAsn1(t, IN_pose->position);
+  asn1Scc_Quaterniond_fromAsn1(q, IN_pose->orientation);
+  // convert quaternion to rotation mat
+  r = q.normalized().toRotationMatrix();
+
+  Eigen::Matrix4d p;
+
+  to4d(r,t,p);
+  
+
+  std::cout << "got position:\n" << p << std::endl;
+}
+
